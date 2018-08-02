643c9b42b42a4027cf8c4f58e22ecdbcb1ce261d
lr17951play <lr17951@qq.com>
2018-08-02 23:33:24 +0800

    made api-server load properties file via config server. note: the config configuration can only work on bootstrap.properties (bootstrap.yml), instead of appliction.properties (appliction.yml).

--- cloud-api/src/main/java/com/lirui/cloud/api/controller/ApiController.java
+++ cloud-api/src/main/java/com/lirui/cloud/api/controller/ApiController.java
@@ -1,5 +1,6 @@
 package com.lirui.cloud.api.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ApiController {
 
+    @Value("${cloud.api.env}")
+    private String env;
+
     @GetMapping("/hello")
     public String hello () {
-        return "This is Api Service";
+        return "This is Api Service and running env is: " + this.env;
     }
 }
